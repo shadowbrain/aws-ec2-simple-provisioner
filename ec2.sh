b433742a4cd892945dd9fb5928056e6b7e026bcf
Brian Butler <shadowbrain@shadowbrain.org>
2015-05-08 16:53:19 -0400

    forget the wait just sleep for a bit, and adding ansible

--- ec2.sh
+++ ec2.sh
@@ -21,14 +21,15 @@ USAGE='Usage: ec2.sh <app_name> <environment> <number_of_instances> <instance_si
 aws ec2 run-instances --image-id ami-5189a661 --count $numInst --instance-type $sizeInst --key-name devops-test --security-groups $envmnt >> ./ec2-script.log
 
 #FIXME: should only look for the new instances
-aws ec2 wait instance-status-ok --include-all-instances
+#aws ec2 wait instance-running --filter Name=instance-type,Values=$sizeInst
 
 
 # Get new instances info
 
 #FIXME: should only include the newly created instances
-aws ec2 decribe-instances | grep -e "LaunchTime" -e "InstanceId" -e "PublicIpAddress"
+sleep 5
+aws ec2 describe-instances | grep -e "LaunchTime" -e "InstanceId" -e "PublicIpAddress"
 
 # Run ansible on the new instances
 
-
+ansible-playbook dev.yml
